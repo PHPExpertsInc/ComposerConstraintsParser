ca728130e91ba6805bff51acf273ff6b35eeeddf
Theodore R. Smith <theodore@phpexperts.pro>
2025-03-25 17:13:37 -0500

    Initial from phpexperts/skeleton.

---
+++ tests/ComposerConstraintsHelperTest.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of Composer Constraints Parser, a PHP Experts, Inc., Project.
+ *
+ * Copyright © 2025 PHP Experts, Inc.
+ * Author: Theodore R. Smith <theodore@phpexperts.pro>
+ *   GPG Fingerprint: 4BF8 2613 1C34 87AC D28F  2AD8 EB24 A91D D612 5690
+ *   https://www.phpexperts.pro/
+ *   https://gitlab.com/hopeseekr/ComposerConstraintsParser
+ *
+ * This file is licensed under the MIT License.
+ */
+
+namespace PHPExperts\ComposerVersionConstraints\Tests;
+
+use Composer\Semver\Constraint\Constraint;
+use Composer\Semver\Semver;
+use Composer\Semver\VersionParser;
+
+class ComposerConstraintsHelperTest extends TestCase
+{
+}
